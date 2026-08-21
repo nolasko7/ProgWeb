d4a7a9728be057a79d55fde68e5bfe97ff3ad37c
Valentin <valentin.nolasco123123@gmail.com>
2026-08-21 11:00:38 -0300

    Rutas implementadas

--- app.js
+++ app.js
@@ -4,6 +4,35 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
+
+//ROUTES
+
+app.get("/", (req, res) => {
+    res.render("pages/index");
+});
+
+app.get("/product/:id", (req, res) => {
+    res.render("pages/product");
+});
+
+app.get("/cart", (req, res) => {
+    res.render("pages/cart");
+});
+
+app.get("/login", (req, res) => {
+    res.render("pages/login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("pages/register");
+});
+
+app.get("/checkout", (req, res) => {
+    res.render("pages/checkout");
+});
+
+//LISTEN
 app.listen(PORT,
     () => console.log("Server is Ready! 🫡")
 )

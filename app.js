91aa36f975433724d81c97851f5abd5a4114f82f
julio <juliovillegasuba@gmail.com>
2026-08-21 17:59:09 -0300

    version 0.2

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express')
 
 const app = express();
 
+const path = require("path");
+
 const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 //ROUTES -
 

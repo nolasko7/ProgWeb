50c2b6dc2be8e8d2d3a80c6d7d98697e07924d10
Valentin <valentin.nolasco123123@gmail.com>
2026-08-23 18:46:03 -0300

    Algunos cambios, medio pelo

--- app.js
+++ app.js
@@ -5,11 +5,27 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
-
+app.use(express.static("public"));
 //ROUTES -
 
 app.get("/", (req, res) => {
-    res.render("pages/index");
+    const productosTec = [{
+        id: 1,
+        nombre: "Notebook Lenovo ThinkPad",
+        precio: 499.99,
+        imagen: "https://images.unsplash.com/photo-1517336714731-4896894b5ce9?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bm90ZWJvb2t8ZW58MHx8MHx8fDA%3D"
+    }, {
+        id: 2,
+        nombre: "Smart TV Samsung 55 pulgadas",
+        precio: 499.99,
+        imagen: "https://images.unsplash.com/photo-1517336714731-4896894b5ce9?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bm90ZWJvb2t8ZW58MHx8MHx8fDA%3D"
+    }, {
+        id: 3,
+        nombre: "Smartphone Xiaomi 128GB",
+        precio: 499.99,
+        imagen: "https://images.unsplash.com/photo-1517336714731-4896894b5ce9?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bm90ZWJvb2t8ZW58MHx8MHx8fDA%3D"
+    },];
+    res.render("pages/index", { productosTec });
 });
 
 app.get("/product/:id", (req, res) => {

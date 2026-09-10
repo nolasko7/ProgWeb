0a332ff269fa8725759c6547eed68f92b42abf13
julio <juliovillegasuba@gmail.com>
2026-09-10 17:25:32 -0300

    listo el carrito, entendia la logica y use la ia para completarlo xddd

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -27,16 +27,64 @@ function addToCart(req, res) {
 function cargarCarrito(req, res) {
     const carrito = req.session.carrito || [];
 
-    const elementosCarrito = carrito.map(objeto=>({
+    const elementosCarrito = carrito
+        .map(item => {
+            const objeto = getProductoPorId(item.productId);
 
-        ...objeto,
+            if (!objeto) {
+                return null;
+            }
 
-        objeto : getProductoPorId(objeto.productId)
+            return {
+                cantidad: Number(item.quantity || 0),
+                objeto,
+                total: Number(item.quantity || 0) * Number(objeto.price)
+            };
+        })
+        .filter(Boolean);
 
+    const totalCarrito = elementosCarrito.reduce((sum, item) => sum + item.total, 0);
 
-    }));
-
-    res.render('pages/cart', { elementosCarrito });
+    res.render('pages/cart', { elementosCarrito, totalCarrito });
 }
 
-module.exports = { addToCart, cargarCarrito };
+function incrementarCantidad(req, res) {
+    const productId = Number(req.params.id);
+    const carrito = req.session.carrito || [];
+    const item = carrito.find(producto => producto.productId === productId);
+
+    if (item) {
+        item.quantity += 1;
+    }
+
+    res.redirect('/cart/list');
+}
+
+function decrementarCantidad(req, res) {
+    const productId = Number(req.params.id);
+    const carrito = req.session.carrito || [];
+
+    req.session.carrito = carrito
+        .map(item => {
+            if (item.productId === productId) {
+                return {
+                    ...item,
+                    quantity: Math.max(0, Number(item.quantity) - 1)
+                };
+            }
+
+            return item;
+        })
+        .filter(item => item.quantity > 0);
+
+    res.redirect('/cart/list');
+}
+
+function quitarProducto(req, res) {
+    const productId = Number(req.params.id);
+    req.session.carrito = (req.session.carrito || []).filter(item => item.productId !== productId);
+
+    res.redirect('/cart/list');
+}
+
+module.exports = { addToCart, cargarCarrito, incrementarCantidad, decrementarCantidad, quitarProducto };
